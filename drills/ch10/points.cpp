ab7eb8016ba589717b8e69e4c1af0a0f06b3faff
karamProg <qaoudkaram58@gmail.com>
2021-04-02 14:41:56 +0200

    ch10

--- drills/ch10/points.cpp
+++ drills/ch10/points.cpp
@@ -1,5 +1,6 @@
 #include "./../std_lib_facilities.h"
 class wrong_input{};
+class size_error{};
 int main () {
 	try {
 		struct Point {
@@ -8,20 +9,19 @@ int main () {
 		};
 
 
-		int x = 0;
-		int y = 0;
+		int x,y;
 		vector<Point> original_points;
-
-		cout << "Enter points in the form of x and y values: e.g 5 7 " << endl;
+		char ch1, ch2, ch3;
+		cout << "Enter points in the form of x and y value pairs: e.g. (5,7) " << endl;
 		int n = 7;
 		while(n>0) {
-			cin >> x >> y;
+			cin >> ch1 >> x >> ch2 >> y >> ch3;
+			if (ch1!='(' || ch2!=',' || ch3!=')') {
+		        throw wrong_input();
+		    }
 			original_points.push_back(Point{x,y});
 			n--;
 		}
-		cout << "Result of original_points vector: " << endl;
-		for (const auto& point : original_points)
-			cout << '(' << point.x << "," << point.y << ')' << endl;
 
 		string oname = "mydata.txt";
 		ofstream ost {oname}; 
@@ -42,11 +42,8 @@ int main () {
 
 
 		vector<Point> processed_points;
-		char ch1, ch2, ch3;
 		string str;
 		Point p;
-		p.x = 0;
-		p.y = 0;
 
 		while(ifs >> ch1 >> x >> ch2 >> y >> ch3) {
 		    if (ch1!='(' || ch2!=',' || ch3!=')') {
@@ -58,15 +55,41 @@ int main () {
 		}
 		//Testing size to see if points were copied. 
 		// cout << "Size of proccessed = " << processed_points.size() << endl;
+
+
+		cout << "Result of original_points vector: " << endl;
+		for (const auto& point : original_points)
+			cout << '(' << point.x << "," << point.y << ')' << endl;
+
+
 		cout << "Result of processed_points vector: " << endl;
 		for (const auto& processed : processed_points) {
 			cout << '(' << processed.x << "," << processed.y << ')'<< endl;
 		}
+		if(original_points.size() != processed_points.size()) {
+			throw size_error();
+		}
+		int counter = 0;
+		for (const auto& point : original_points) {
+			if(point.x != processed_points[counter].x || point.y != processed_points[counter].y) {
+				cout << "Something is wrong, boy!" <<endl;
+				break;
+			}
+			counter++;
+		}
+
+
 	}
 
 	catch (exception& e) {
-	    cerr << "Error: " << e.what() << '\n';
+	    cerr << "Error: " << e.what() << endl;
 	    	return 1;
+	}
+	catch (size_error&) {
+		cerr << "ERROR: The sizes of the vectors are not equal!!" << endl;
+	}
+	catch (wrong_input&) {
+		cerr << "ERROR: Data was not inputted properly!" << endl;
 	}
 	catch (...) {
     	cerr << "Something terrible has happened!\n";
